4ad9bd3fb055e8f653fa324694456fc0fee91010
kunkka0822 <hungryAdmiral0512@gmail.com>
2024-10-16 01:16:23 +0200

    feat: update price service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PriceService } from './price/price.service';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService, PriceService, PrismaService],
+  controllers: [],
+  providers: [PriceService, PrismaService],
 })
 export class AppModule {}
--- src/price/price.service.ts
+++ src/price/price.service.ts
@@ -1,16 +1,30 @@
-import { Timeout } from '@nestjs/schedule';
 import { Injectable, Logger } from '@nestjs/common';
+import anchor, { Program } from '@coral-xyz/anchor';
 
 import { PrismaService } from 'src/prisma/prisma.service';
+import idl from 'idl/solana_program.json';
 
 @Injectable()
 export class PriceService {
   private readonly logger = new Logger(PriceService.name);
 
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) {
+    this.initialize();
+  }
 
-  @Timeout(30_000)
   async initialize() {
-    // TODO
+    console.log('Pool Service initialized..');
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+
+    //@ts-ignore
+    const program = new Program(idl, provider);
+    program.addEventListener('purchaseEvent', (event: any) => {
+      console.log(event, {
+        reserveSol: event.reserveSol.toString(),
+        reserveToken: event.reserveToken.toString(),
+        totalSupply: event.totalSupply.toString(),
+      });
+    });
   }
 }
